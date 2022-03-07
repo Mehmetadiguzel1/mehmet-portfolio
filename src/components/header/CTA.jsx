e2dfa80c0e7b6f9b7273de72abc36d3ac7f7ca54
Mehmet Furkan Adiguzel <55146992+Mehmetadiguzel1@users.noreply.github.com>
2022-03-07 00:56:05 -0500

    Header for CTA created. CV added for download

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CV from "../../assets/Mehmet-cv.pdf";
+const CTA = () => {
+  return (
+    <div className="cta">
+      <a href={CV} download>
+        Download Resume
+      </a>
+      <a href="#contact">Let's Talk</a>
+    </div>
+  );
+};
+
+export default CTA;
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import "./header.css";
 const header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Mehmet Adiguzel</h1>
+        <h5 className="text-light">Frond end Developer</h5>
+      </div>
+    </header>
+  );
 };
 
 export default header;
