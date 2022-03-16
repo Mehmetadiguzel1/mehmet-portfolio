36b9f8bc7884da146f9975b7d820a10d0b3ec1d6
Mehmet Furkan Adiguzel <55146992+Mehmetadiguzel1@users.noreply.github.com>
2022-03-15 22:49:25 -0500

    Footer section done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,59 @@
 import React from "react";
 import "./footer.css";
+import {
+  AiFillInstagram,
+  AiFillTwitterCircle,
+  AiFillFacebook,
+} from "react-icons/ai";
+import { BsDiscord } from "react-icons/bs";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer__logo"></a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#service">Service</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.instagram.com/m.f.adgzl/">
+          <AiFillInstagram />
+        </a>
+        <a href="">
+          <AiFillFacebook />
+        </a>
+        <a href="">
+          <AiFillTwitterCircle />
+        </a>
+        <a href="">
+          <BsDiscord />
+        </a>
+      </div>
+      <div className="footer__copyright">
+        <small> &copy; MEHMET ADIGUZEL. All rights reserved </small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
